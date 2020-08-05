8536cc5a2bd5d964f748a9f025edfa57f5576470
flyseddy <sedrick100thomas@gmail.com>
2020-08-05 15:54:05 -0400

    Project Structure

---
+++ custom_playlist.py
@@ -0,0 +1,25 @@
+# Method for receiving user input
+# Method for making the API endpoint request for artists ID
+# Method for making the API endpoint request for recommendations
+
+class CreateCustomPlaylist:
+
+    def __init__(self):
+        pass
+
+    def receive_user_input(self):
+        pass
+
+    def make_api_artist_request(self):
+        pass
+
+    def make_recommend_api_request(self):
+        pass
+
+    def create_playlist(self):
+        pass
+
+    def add_items_to_playlist(self):
+        pass
+
+
