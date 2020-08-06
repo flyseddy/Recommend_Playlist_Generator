d945ceb0602dac5b0859ef55b15173fabd7a2fb4
flyseddy <sedrick100thomas@gmail.com>
2020-08-05 23:50:04 -0400

    First Demo(Working)

--- custom_playlist.py
+++ custom_playlist.py
@@ -2,14 +2,14 @@
 # Method for making the API endpoint request for artists ID
 # Method for making the API endpoint request for recommendations
 import requests
-from config import spotify_artist_token
+from config import spotify_artist_token, spotify_rec_token
 
 class CreateCustomPlaylist:
 
     def __init__(self):
         self.artist = input("Enter an artist: ")
         self.genre = input("Enter a genre: ")
-        self.track = input("Enter a track: ")
+        # self.track = input("Enter a track: ")
 
     def make_api_artist_request(self):
         endpoint_artist_url = 'https://api.spotify.com/v1/search?'
@@ -20,10 +20,21 @@ class CreateCustomPlaylist:
                                         headers = {"Content-Type": "application/json",
                                                     "Authorization": "Bearer {}".format(spotify_artist_token)})
         json_artist_response = artist_response.json()
-        return json_artist_response['artists']['items'][0]['uri'].replace('spotify:artist:', '')                                            
+        artist = json_artist_response['artists']['items'][0]['uri'].replace('spotify:artist:', '') 
+        return artist                                           
 
-    def make_recommend_api_request(self):
-        pass
+    def make_recommend_api_request(self, artist_new):
+        endpoint_rec_url = 'https://api.spotify.com/v1/recommendations?'
+        query = f'{endpoint_rec_url}limit={10}&seed_artists={artist_new}&seed_genres={self.genre}'
+
+        recommend_response = requests.get(query,
+                                            headers = {"Content-Type": "application/json",
+                                                        "Authorization": "Bearer {}".format(spotify_rec_token)})
+        json_rec_response = recommend_response.json()
+        for item in json_rec_response['tracks']:
+            artist_name = item['album']['artists'][0]['name']
+            artist_song = item['name']
+            print(f"{artist_name} - {artist_song}")
 
     def create_playlist(self):
         pass
@@ -31,7 +42,11 @@ class CreateCustomPlaylist:
     def add_items_to_playlist(self):
         pass
 
+
 if __name__ == "__main__":
     playlist = CreateCustomPlaylist()
-    print(playlist.make_api_artist_request()) 
+    # playlist.make_api_artist_request()
+    artist = playlist.make_api_artist_request()
+    playlist.make_recommend_api_request(artist)
+     
 
